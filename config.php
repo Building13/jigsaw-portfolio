57514de3416b6079de63520e7fa83c02089950a4
Nathan Gross <nathan@bldg13.com>
2021-03-16 15:41:31 -0400

    initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => 'http://jigsaw-portfolio.test',
+    'title' => 'Jigsaw Portfolio',
+    'description' => 'This is a portfolio site built on Jigsaw.',
+    'collections' => [],
+];
